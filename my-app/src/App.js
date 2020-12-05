e2d37a4d2077e00de920fdb88a815ab73785678f
Mian Uddin <mianuddin@gmail.com>
2020-12-04 20:39:47 -0800

    feat: add toggle

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [val, setVal] = useState("");
   const [resp, setResp] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [toggle, setToggle] = useState(false);
   const handleClick = async () => {
     setResp(null);
     setLoading(true);
@@ -33,9 +34,9 @@ function App() {
       {resp && (
         <>
           <h2>Response</h2>
-          <p>The server returned <span className="length">{resp.compressions ? resp.compressions.length : 0}</span> possible compressions. {resp.compressions && "Here are the top compressions:"}</p>
+          <p>The server returned <span className="length">{resp.compressions ? resp.compressions.length : 0}</span> possible compressions. {resp.compressions && <span>Here are <a className="toggle" onClick={() => setToggle(!toggle)}>{toggle ? "all the": "the top"}</a> compressions:</span>}</p>
           {resp.compressions &&
-            <div>{resp.compressions.slice(0, 5).map((c, i) => <div className="pair"><span className="prob">{Math.round((c[0] + Number.EPSILON) * 100) / 100}</span><span>{c[1]}</span></div>)}</div>
+            <div>{resp.compressions.slice(0, toggle ? resp.compressions.length : 5).map((c, i) => <div className="pair"><span className="prob">{Math.round((c[0] + Number.EPSILON) * 100) / 100}</span><span>{c[1]}</span></div>)}</div>
           }
           <p className="srvmsg">{resp.msg}</p>
         </>
